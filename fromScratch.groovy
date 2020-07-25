06940f35a35a9c200adb7f2d1fea9a76bba29fe8
DariaLarkin <larkin.daria@gmail.com>
2020-07-25 11:16:20 -0500

    newJob

--- fromScratch.groovy
+++ fromScratch.groovy
@@ -6,24 +6,32 @@ node {
       [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], 
       
       // below line triggers this job every minute
-      pipelineTriggers([pollSCM('* * * * *') // that is mean, read the github and if see some changes - perform the task, if not do not do nothing
-    ])
-  ])
-
+      pipelineTriggers([pollSCM('* * * * *')])]) // that is mean, read the github and if see some changes - perform the task, if not do not do nothing
+  
 
    stage("Pull Repo"){
        git "https://github.com/DariaLarkin/FromScratch.git"
    }
-stage("Stage1"){
-       echo "hello"
+  stage("Install Prerequisites"){
+      // in Groovy language """ in the begining and """ in the end mean i wanna put multiple commands
+       sh """   
+       ssh centos@jenkins_worker1.acirrustech.com          sudo yum install httpd -y
+      
+       """
    }
-   stage("Stage2"){
-       echo "hello"
+   stage("Copy artifacts"){
+    shh """
+       scp -r centos@jenkins_worker1.acirrustech.com:/tmp
+       ssh centos@jenkins_worker1.acirrustech.com              sudo cp -r /tmp/index.html /var/www/html
+       ssh centos@jenkins_worker1.acirrustech.com              sudo cp /tmp/style.css /var/www/html
+       ssh centos@jenkins_worker1.acirrustech.com              sudo chown centos:centos /var/www/html
+       ssh centos@jenkins_worker1.acirrustech.com              sudo chmos 777 /var/www/html
+       """
    }
-   stage("Stage3"){
-       echo "hello"
+   stage("Restart web server"){
+       sh "ssh centos@jenkins_worker1.acirrustech.com              sudo systemctl restart httpd"
    }
-   stage("Stage4"){
-       echo "hello"
+   stage("Slack"){
+       slacksend color: '#BADA55', message: 'Hello, World'
    }
 }
